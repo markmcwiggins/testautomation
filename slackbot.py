6dab32609b23abd723ed105c75c9a01b302ec391
Mark McWiggins <mark@pythonsoftwarewa.com>
2023-08-28 15:20:20 -0700

    got slackbot workinggit add slackbot.py

---
+++ slackbot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+from slack_sdk import WebClient
+
+# Set your OAuth Access Token
+SLACK_TOKEN = os.getenv('SLACK_TOKEN')
+
+# Initialize the Slack WebClient
+client = WebClient(token=SLACK_TOKEN)
+
+def send_message(user_id, message):
+    try:
+        response = client.chat_postMessage(
+            channel=user_id,
+            text=message
+        )
+        if response['ok']:
+            print("Message sent successfully!")
+        else:
+            print("Failed to send message.")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+if __name__ == "__main__":
+    user_id = "test-automation"  # Replace with the user's Slack ID
+    message = "Hello, this is a test message!"
+    send_message(user_id, message)
