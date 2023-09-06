e60aa40a7835320b813cfa5acf6425900834c460
Mark McWiggins <mark@pythonsoftwarewa.com>
2023-09-06 14:48:28 -0700

    release 1.0

--- slackbot.py
+++ slackbot.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import os
+import sys
 from slack_sdk import WebClient
+
+slackuser = sys.argv[1]
+hash = sys.argv[2]
 
 # Set your OAuth Access Token
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
@@ -8,10 +12,10 @@ SLACK_TOKEN = os.getenv('SLACK_TOKEN')
 # Initialize the Slack WebClient
 client = WebClient(token=SLACK_TOKEN)
 
-def send_message(user_id, message):
+def send_message(channel, message):
     try:
         response = client.chat_postMessage(
-            channel=user_id,
+            channel=channel,
             text=message,
             username='<mark>'
         )
@@ -23,6 +27,6 @@ def send_message(user_id, message):
         print("An error occurred:", str(e))
 
 if __name__ == "__main__":
-    user_id = "test-automation"  # Replace with the user's Slack ID
-    message = "user <mark> broke the build with commit sdflkjsdfl"
-    send_message(user_id, message)
+    channel = "test-automation" 
+    message = "user <%s> broke the build with commit <%s>" % (slackuser, hash)
+    send_message(channel, message)
